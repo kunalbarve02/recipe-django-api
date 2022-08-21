29950eaa1c0f30bdeaa28e17235fdcfd4f8fb3cb
Kunal Barve <kunalbarve02@gmail.com>
2022-08-21 12:53:41 +0530

    added psotgresql

---
+++ app/app/tests.py
@@ -0,0 +1,7 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res=calc.add(5,6)
+        self.assertEqual(res,11)
