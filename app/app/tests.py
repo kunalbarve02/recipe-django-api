d537fc700e1afd2b48b47fcc8ef6dbb878ffcba8
Kunal Barve <kunalbarve02@gmail.com>
2022-08-21 13:02:32 +0530

    added psotgresql fixed linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,8 @@
 from django.test import SimpleTestCase
 from app import calc
 
-class CalcTests(SimpleTestCase):
-    def test_add_numbers(self):
-        res=calc.add(5,6)
-        self.assertEqual(res,11)
+
+class CalcTests( SimpleTestCase ):
+    def test_add_numbers( self ):
+        res=calc.add( 5,6 )
+        self.assertEqual( res,11 )
